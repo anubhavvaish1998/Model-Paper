7e537b2c9503c41cc37a42e53743dd6a019456aa
Anubhav Vaish <anubhavvaish04@gmail.com>
2024-05-22 00:05:53 +0530

    second commit

--- Model Paper/Controllers/HomeController.cs
+++ Model Paper/Controllers/HomeController.cs
@@ -7,10 +7,12 @@ namespace Model_Paper.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -18,14 +20,28 @@ namespace Model_Paper.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult Privacy()
         {
             return View();
         }
 
-        public void Upoload(IFormFile file)
-        { 
-        
+        [HttpPost]
+        public async Task<IActionResult> Privacy(IFormFile file)
+        {
+            string uploadFolder = Path.Combine(_webHostEnvironment.WebRootPath,"Uploads");
+            if (!Directory.Exists(uploadFolder))
+            {
+                Directory.CreateDirectory(uploadFolder);
+            }
+            string fileName = Path.GetFileName(file.FileName);
+            string fileSavePath = Path.Combine(uploadFolder,fileName);
+            using (FileStream stream = new FileStream(fileSavePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            ViewBag.Message = fileName + "File Saved Successfuly";
+            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Model Paper/obj/Debug/net8.0/Model Paper.AssemblyInfo.cs
+++ Model Paper/obj/Debug/net8.0/Model Paper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Model Paper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1147791558e2fde1913e91b24cb4de08506bf482")]
 [assembly: System.Reflection.AssemblyProductAttribute("Model Paper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Model Paper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
